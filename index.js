238d3796d3708a975fb59117ec51d420279b2e88
unknown <brennowers@gmail.com>
2025-03-19 00:37:01 -0500

    added hamburger menu on mobile, added booking links

--- index.js
+++ index.js
@@ -7,19 +7,38 @@ const swiper = new Swiper('.swiper', {
     autoplay: {
         delay: 2500
     },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next-unique',
-        prevEl: '.swiper-button-prev-unique',
+    navigation: false,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
     },
-
     // Responsive breakpoints
     breakpoints: {
         // when window width is >= 640px
-        1000: {
+        1140: {
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next-unique',
+                prevEl: '.swiper-button-prev-unique',
+            },
             slidesPerView: 2,
             spaceBetween: 20
         }
     }
 });
+
+const hamburger = document.getElementById("hamburger-menu");
+const navMenu = document.getElementById("nav-menu");
+
+document.getElementById("hamburger-menu").addEventListener("click", (e) => {
+    navMenu.classList.add("open");
+    hamburger.classList.add("nav-open");
+})
+
+document.addEventListener('click', (e) => {
+    let clickInside = navMenu.contains(e.target) || (hamburger.contains(e.target) && hamburger.classList.contains("nav-open"))
+  
+    if (!clickInside) {
+        navMenu.classList.remove('open')
+    }
+  })
